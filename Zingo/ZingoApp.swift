821b56d398945becd9e4fbdc74a33327a99ac3b7
Bogdan Zyk <zykovbogdan07@gmail.com>
2023-05-19 15:54:31 +0300

    Initial Commit

---
+++ Zingo/ZingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZingoApp.swift
+//  Zingo
+//
+//  Created by Bogdan Zykov on 19.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
